cbb84339303c3266bb8a5402252abe77e8dd201e
Julia Marco <marcootj@gmail.com>
2019-03-12 14:45:41 -0600

    Added finder spec and controller to return an object representation

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -15,4 +15,46 @@ describe "Merchants API" do
 
     expect(merchants.count).to eq(4)
   end
+
+  it "can get one merchant by its id" do
+    m = create(:merchant, id: 64, name: "Koepp, Waelchi and Donnelly", created_at: "2012-03-27 14:54:05 UTC", updated_at: "2012-03-27 14:54:05 UTC")
+    id = m.id
+
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+  end
+
+  it "can get one merchant using finders" do
+    m = create(:merchant, id: 5, name: "Williamson Group", created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
+    id = m.id
+    name = m.name
+    created_at = m.created_at
+    updated_at = m.updated_at
+
+    get "/api/v1/merchants/find?id=#{id}"
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id)
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    expect(response).to be_successful
+    expect(merchant["name"]).to eq(name)
+
+    get "/api/v1/merchants/find?created_at=#{created_at}"
+
+    expect(response).to be_successful
+    expect(merchant["created_at"]).to eq("2012-03-27T14:53:59.000Z")
+
+    get "/api/v1/merchants/find?updated_at=#{updated_at}"
+
+    expect(response).to be_successful
+    expect(merchant["updated_at"]).to eq("2012-03-27T14:53:59.000Z")
+
+  end
+
 end
